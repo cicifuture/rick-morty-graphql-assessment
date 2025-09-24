ce22d1eff4f23b9022b80b2fd090b7e6ec25e1df
Cici Liu <cici@futurerent.com.au>
2025-09-24 16:40:51 +1000

    feat: persist cache and await restore to avoid loading flashes and speed up reloads

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,25 +1,16 @@
 // Apollo client initalization
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
 
 const cache = new InMemoryCache();
 
-(async () => {
-  try {
-    await persistCache({
-      cache,
-      storage: new LocalStorageWrapper(window.localStorage),
-    });
-  } catch (error) {
-    console.error("Error restoring Apollo cache", error);
-  }
-})();
+await persistCache({ cache, storage: new LocalStorageWrapper(localStorage) });
 
 export const client: ApolloClient = new ApolloClient({
   link: new HttpLink({ uri: "https://rickandmortyapi.com/graphql" }),
   cache,
+  defaultOptions: {
+    watchQuery: { fetchPolicy: "cache-first", nextFetchPolicy: "cache-first" },
+    query: { fetchPolicy: "cache-first" },
+  },
 });
